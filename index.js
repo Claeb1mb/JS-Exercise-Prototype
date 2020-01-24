fc0fce3ea0f3816891930776b30e7daf24a6edb0
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2020-01-23 20:05:58 -0500

    done again

--- index.js
+++ index.js
@@ -87,13 +87,16 @@ Car.prototype.fill = function(gallons) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
   this.name = name;
   this.age = age;
   this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
 Baby.prototype.play = function() {
   return "Playing with ${this.favoriteToy}";
 };
+
 /* 
   TASK 4
 
